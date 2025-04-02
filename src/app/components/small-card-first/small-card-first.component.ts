405e4118f32f3a3aa53ff55165c75e54a3c89f0b
LeandroMeca <leandromeca20@outlook.com>
2025-04-01 22:59:20 -0300

    created big card

---
+++ src/app/components/small-card-first/small-card-first.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card-first',
+  standalone: true,
+  imports: [],
+  templateUrl: './small-card-first.component.html',
+  styleUrl: './small-card-first.component.css'
+})
+export class SmallCardFirstComponent {
+
+}
---
+++ src/app/components/small-card-second/small-card-second.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-small-card-second',
+  standalone: true,
+  imports: [],
+  templateUrl: './small-card-second.component.html',
+  styleUrl: './small-card-second.component.css'
+})
+export class SmallCardSecondComponent {
+
+}
