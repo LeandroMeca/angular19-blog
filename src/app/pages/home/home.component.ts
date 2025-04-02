cd8f61d2d0cb1c05cb4465ffb0c1748610921229
LeandroMeca <leandromeca20@outlook.com>
2025-04-02 17:00:30 -0300

    finished small card

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
 import { BigCardComponent } from "../../components/big-card/big-card.component";
+import { SmallCardFirstComponent } from "../../components/small-card-first/small-card-first.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuTitleComponent, BigCardComponent],
+  imports: [MenuTitleComponent, BigCardComponent, SmallCardFirstComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
