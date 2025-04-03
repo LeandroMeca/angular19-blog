d64bf52f91b0172af390b1585ae3a896a1c946cb
LeandroMeca <leandromeca20@outlook.com>
2025-04-02 23:58:02 -0300

    header finished with buttons

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { MenuTitleComponent } from "../../components/menu-title/menu-title.component";
 import { BigCardComponent } from "../../components/big-card/big-card.component";
 import { SmallCardFirstComponent } from "../../components/small-card-first/small-card-first.component";
+import { MenuBarComponent } from '../../components/menu-bar/menu-bar.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MenuTitleComponent, BigCardComponent, SmallCardFirstComponent],
+  imports: [MenuTitleComponent, BigCardComponent, SmallCardFirstComponent, MenuBarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
