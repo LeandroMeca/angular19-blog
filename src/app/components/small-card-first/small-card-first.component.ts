25017f8ff8c8369cbe6e66ea0c3f7fb9c141f68b
LeandroMeca <leandromeca20@outlook.com>
2025-04-04 02:58:35 -0300

    page dinamic and project finished

--- src/app/components/small-card-first/small-card-first.component.ts
+++ src/app/components/small-card-first/small-card-first.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-small-card-first',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './small-card-first.component.html',
   styleUrl: './small-card-first.component.css'
 })
@@ -17,5 +18,9 @@ export class SmallCardFirstComponent {
 
   @Input() 
   cardDescription:string = ""
+  
+  @Input() 
+  id:string = "0";
+
 
 }
--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { dataFake } from "../../data/dataFake";
 
 @Component({
   selector: 'app-content',
@@ -10,20 +11,33 @@ import { RouterLink } from '@angular/router';
 })
 export class ContentComponent implements OnInit{
 
-  @Input()
+ 
   photoCover:string = "";
-  @Input()
+
   contentTitle:string = ""
-  @Input()
+
   contentDescription:string = ""
 
+  private id:string | null = "0"
 
-  constructor(){}
+
+  constructor(private route:ActivatedRoute){}
 
 
   ngOnInit(): void {
-   
+   this.route.paramMap.subscribe( value => console.log(value.get("id"))
+   )
+   this.setValuesToComponent(this.id)
   }
 
 
+  setValuesToComponent(id:string | null){
+    const result = dataFake.filter(article => article.id == id)[0]
+  
+      this.contentTitle = result.title
+      this.contentDescription = result.description
+      this.photoCover = result.photoCover
+
+    
+  }
 }
