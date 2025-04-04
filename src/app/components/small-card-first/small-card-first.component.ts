19e216d988a34be5b761e4205e72d2854943a5a5
LeandroMeca <leandromeca20@outlook.com>
2025-04-03 22:39:13 -0300

    Desacoplando os componentes da parte visual

--- src/app/components/small-card-first/small-card-first.component.ts
+++ src/app/components/small-card-first/small-card-first.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card-first',
@@ -8,5 +8,14 @@ import { Component } from '@angular/core';
   styleUrl: './small-card-first.component.css'
 })
 export class SmallCardFirstComponent {
+
+  @Input()
+  photoCover:string = ""
+
+  @Input()
+  cardTitle:string = ""
+
+  @Input() 
+  cardDescription:string = ""
 
 }
